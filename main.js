9324bb7610140c1926dd3903e5a7bcadda18fa1f
Arakunle Peace <arakunlepeacesamuel@gmail.com>
2022-06-21 18:58:55 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+
+// ########## Using Function ###########
+
+// Declare My Name
+function getName() {
+    alert('My Name Is Arakunle Peace Samuel')
+}
+
+// Declare My Height
+function getHeight() {
+    alert("I am Of Height " + 180.34 + "cm")
+}
+
+// Declare My Country
+function getCountry() {
+    alert("I Am From Nigeria")
+}
+
+// print out my declaration to the screen
+getName();
+getHeight();
+getCountry();
